667135135ecb879c9af800e54b841009db027ea1
Sebastian Olaru <sebastianolaru3008@gmail.com>
2020-01-06 23:05:45 +0200

    am adaugat tot cmake-build-debug floder-ul ca sa fie toate fisierele nesesare in repo

---
+++ cmake-build-debug/CMakeFiles/food_ordering.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/acer/Desktop/CP Lab/food-ordering/foodData.c" "C:/Users/acer/Desktop/CP Lab/food-ordering/cmake-build-debug/CMakeFiles/food_ordering.dir/foodData.c.obj"
+  "C:/Users/acer/Desktop/CP Lab/food-ordering/input.c" "C:/Users/acer/Desktop/CP Lab/food-ordering/cmake-build-debug/CMakeFiles/food_ordering.dir/input.c.obj"
+  "C:/Users/acer/Desktop/CP Lab/food-ordering/main.c" "C:/Users/acer/Desktop/CP Lab/food-ordering/cmake-build-debug/CMakeFiles/food_ordering.dir/main.c.obj"
+  "C:/Users/acer/Desktop/CP Lab/food-ordering/print.c" "C:/Users/acer/Desktop/CP Lab/food-ordering/cmake-build-debug/CMakeFiles/food_ordering.dir/print.c.obj"
+  "C:/Users/acer/Desktop/CP Lab/food-ordering/signIn.c" "C:/Users/acer/Desktop/CP Lab/food-ordering/cmake-build-debug/CMakeFiles/food_ordering.dir/signIn.c.obj"
+  "C:/Users/acer/Desktop/CP Lab/food-ordering/userData.c" "C:/Users/acer/Desktop/CP Lab/food-ordering/cmake-build-debug/CMakeFiles/food_ordering.dir/userData.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/food_ordering.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/food_ordering.dir/foodData.c.obj"
+  "CMakeFiles/food_ordering.dir/input.c.obj"
+  "CMakeFiles/food_ordering.dir/main.c.obj"
+  "CMakeFiles/food_ordering.dir/print.c.obj"
+  "CMakeFiles/food_ordering.dir/signIn.c.obj"
+  "CMakeFiles/food_ordering.dir/userData.c.obj"
+  "food_ordering.exe"
+  "food_ordering.exe.manifest"
+  "food_ordering.pdb"
+  "libfood_ordering.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/food_ordering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
